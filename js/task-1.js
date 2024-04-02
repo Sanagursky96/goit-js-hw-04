0876e1f1aec86a9dc12cb0dcb92596d892a6ad04
Sanagursky96 <usanagur96@gmail.com>
2024-04-03 00:17:56 +0300

    commit

--- js/task-1.js
+++ js/task-1.js
@@ -5,7 +5,7 @@ function isEnoughCapacity(products, containerSize) {
     total += product;
   }
 
-  return total < containerSize;
+  return total <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
